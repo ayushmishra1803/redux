efea3a4c5e471008cffae8fd8243835640ef67ff
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-10-03 23:33:30 +0530

    Updates

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import reducer from "./Store/reducer";
+import {Provider} from "react-redux"
 const store = createStore(reducer);
 ReactDOM.render(
 	<React.StrictMode>

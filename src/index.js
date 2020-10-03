ee4bba18f898c8cf4552db0ab2cb2702bcb65d9e
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-10-03 23:32:11 +0530

    Updartes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {createStore} from "redux"
-
-const store = createStore();
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import reducer from "./Store/reducer";
+const store = createStore(reducer);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root"),
 );
-
 
 serviceWorker.unregister();

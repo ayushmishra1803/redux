3851712011bfbf881eec83469b37001f090e7b40
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-10-08 04:10:33 +0530

    Updates

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from "./Store/reducer";
 import { Provider } from "react-redux";
-const store = createStore(reducer);
+
+const logger = (store) => {
+	return (next) => {
+		return (action) => {
+			console.log("Hello Middle Ware", action);
+			next(action);
+			console.log(store.getState());
+		};
+	};
+};
+
+const store = createStore(reducer, applyMiddleware(logger));
+
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
